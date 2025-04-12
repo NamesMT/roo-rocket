3211ad62dee4ac4b3d9b3ecc8ff2443b5318366c
NamesMT <dangquoctrung123@gmail.com>
2025-04-12 12:16:41 +0000

    feat: MVP unpacking from url

--- libs/roo-rocket/src/cli.ts
+++ libs/roo-rocket/src/cli.ts
@@ -1,15 +1,70 @@
+import { rm } from 'node:fs/promises'
+import simpleWriteFileWithDirs from '@local/common/src/utils/fs/simpleWriteFileWithDirs'
 import { defineCommand, runMain } from 'citty'
+import { strFromU8, unzip } from 'fflate'
+import { join } from 'pathe'
 import { logger } from '~/helpers/logger'
+import { rocketAssemble } from '~/rocket/assemble'
+
+async function unpackFromUrl(url: string) {
+  logger.info(`Downloading config pack from ${url}`)
+
+  const res = await fetch(url)
+
+  if (!res.ok)
+    throw new Error(`Failed to download config pack from ${url}`)
+
+  const configPackBuffer = new Uint8Array(await res.arrayBuffer())
+
+  logger.start('Extracting config pack to `.tmp`...')
+  await new Promise<void>((resolvePromise, rejectPromise) => {
+    unzip(configPackBuffer, (err, unzipped) => {
+      if (err) {
+        return rejectPromise(new Error('Failed to extract the config pack.'))
+      }
+
+      for (const [key, value] of Object.entries(unzipped))
+        simpleWriteFileWithDirs(join('.tmp', key), strFromU8(value))
+
+      resolvePromise()
+    })
+  })
+  logger.success('Extracted successfully.')
+
+  logger.start('Assembling the config according to `rocketConfig`...')
+  await rocketAssemble({
+    frameDir: join('.tmp', 'frame'),
+    fuelDir: join('.tmp', 'fuel'),
+    outDir: '.',
+  })
+  logger.start('Assembled successfully, removing temporary files...')
+  await rm('.tmp', { recursive: true })
+  logger.success('All done, enjoy your new config!')
+}
 
 const main = defineCommand({
+  meta: {
+    name: 'roo-rocket',
+    description: 'Roo Rocket CLI',
+  },
   args: {
-    friendly: {
-      type: 'boolean',
-      description: 'Use friendly greeting',
+    url: {
+      type: 'string',
+      description: 'The direct URL to the config pack',
+    },
+    repo: {
+      type: 'string',
+      description: 'The github repository slug (e.g: NamesMT/roo-rocket)',
     },
   },
-  run({ args }) {
-    logger.log(`${args.friendly ? 'Hi' : 'Greetings'}!`)
+  async run({ args }) {
+    const { url, repo } = args
+
+    if (!url && !repo)
+      throw new Error('`url` or `repo` is required')
+
+    if (url)
+      return await unpackFromUrl(url)
   },
 })
 
