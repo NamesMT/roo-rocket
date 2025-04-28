382a5880ec0cf6d371c6d0aa36e5649b6028cc90
NamesMT <dangquoctrung123@gmail.com>
2025-04-28 14:45:35 +0000

    fix: improve the merge hooks to be more generic and future proof

--- libs/roo-rocket/src/rr/hookable.ts
+++ libs/roo-rocket/src/rr/hookable.ts
@@ -7,6 +7,7 @@ import type { UnpackOptions } from 'config-rocket/cli'
 import { readFile } from 'node:fs/promises'
 import defu from 'defu'
 import { createHooks } from 'hookable'
+import { basename } from 'pathe'
 import { logger } from '~/helpers/logger'
 
 /**
@@ -23,49 +24,52 @@ export function createHookable(): NonNullable<UnpackOptions['hookable']> {
     }
   })
 
-  // Hooks to handle .roomodes merging
+  // Hook to allow .roomodes to be merged (cause it's .json)
   instance.hook('onFileOutput', async (state) => {
     if (state.filePath.endsWith('.roomodes')) {
       state.mergeType = 'json'
       state.isValidFileToMerge = true
     }
   })
+
+  // Hook to handle modes and mcp json merging
   instance.hook('onFileOutputJsonMerge', async (state) => {
-    if (state.filePath.endsWith('.roomodes')) {
-      const existingModeSlugs = new Set<string>()
-      const oldData = JSON.parse(await readFile(state.filePath, 'utf8'))
+    if (basename(state.filePath).includes('mode')) {
       const newData = JSON.parse(state.data)
-      const mergedData = defu(newData, oldData)
-      const dedupedModes = mergedData.customModes.filter((mode: any) => {
-        if (existingModeSlugs.has(mode.slug)) {
-          logger.info(`Present roomode entry overwritten: ${mode.slug}`)
-          return false
-        }
+      if (newData.customModes) {
+        const existingModeSlugs = new Set<string>()
+        const oldData = JSON.parse(await readFile(state.filePath, 'utf8'))
+        const mergedData = defu(newData, oldData)
+        const dedupedModes = mergedData.customModes.filter((mode: any) => {
+          if (existingModeSlugs.has(mode.slug)) {
+            logger.info(`Present roomode entry overwritten: ${mode.slug}`)
+            return false
+          }
 
-        return existingModeSlugs.add(mode.slug) && true
-      })
+          return existingModeSlugs.add(mode.slug) && true
+        })
 
-      const result = { ...mergedData, customModes: dedupedModes }
-      state.mergeResult = JSON.stringify(result, null, 2)
-    }
-  })
-
-  // Hook to handle mcp.json merging
-  instance.hook('onFileOutputJsonMerge', async (state) => {
-    if (state.filePath.endsWith('.roo/mcp.json')) {
-      const oldData = JSON.parse(await readFile(state.filePath, 'utf8'))
-      const newData = JSON.parse(state.data)
-      const mergedData = structuredClone(newData)
-      for (const [key, value] of Object.entries(oldData.mcpServers)) {
-        if (key in mergedData.mcpServers) {
-          logger.info(`Present mcp server entry overwritten: ${key}`)
-          continue
-        }
-
-        mergedData.mcpServers[key] = value
+        const result = { ...mergedData, customModes: dedupedModes }
+        state.mergeResult = JSON.stringify(result, null, 2)
       }
+    }
 
-      state.mergeResult = JSON.stringify(mergedData, null, 2)
+    if (basename(state.filePath).includes('mcp')) {
+      const newData = JSON.parse(state.data)
+      if (newData.mcpServers) {
+        const oldData = JSON.parse(await readFile(state.filePath, 'utf8'))
+        const mergedData = structuredClone(newData)
+        for (const [key, value] of Object.entries(oldData.mcpServers)) {
+          if (key in mergedData.mcpServers) {
+            logger.info(`Present mcp server entry overwritten: ${key}`)
+            continue
+          }
+
+          mergedData.mcpServers[key] = value
+        }
+
+        state.mergeResult = JSON.stringify(mergedData, null, 2)
+      }
     }
   })
 
